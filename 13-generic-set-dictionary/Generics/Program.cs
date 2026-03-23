378b71b5f5bda63ce984c832885b8674ec47aaea
Daiane S Fernandes <dsilvafernandes18@gmail.com>
2026-03-23 17:54:53 -0300

    Atividades de Generic, set e dictionary

---
+++ 13-generic-set-dictionary/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main (string [] agrs)
+        {
+            PrintServices<int> printServices = new PrintServices<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            for(int i = 0 ; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine()!);
+                printServices.AddValue(x);
+            }
+            int a = printServices.First();
+            int b = a + 2;
+            Console.WriteLine(a + " + 2 = " + b);
+
+            printServices.Print();
+            Console.WriteLine("First: " + printServices.First());
+
+        }
+    }
+}
